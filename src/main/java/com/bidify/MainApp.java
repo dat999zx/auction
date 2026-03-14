bd67a20437fef34452fe93eb37a7aac182ea670c
dat999zx <dat999zx@gmail.com>
2026-03-14 10:46:04 +0700

    fixed css login controller

--- src/main/java/com/bidify/MainApp.java
+++ src/main/java/com/bidify/MainApp.java
@@ -10,6 +10,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
         Scene scene = new Scene(loader.load(), 1200, 800);
+        scene.getStylesheets().add(MainApp.class.getResource("/css/login.css").toExternalForm());
         stage.setTitle("Bidify");
         stage.setMinWidth(1280);
         stage.setMaxWidth(1280);
--- src/main/java/com/bidify/controller/LoginController.java
+++ src/main/java/com/bidify/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.bidify.controller;
 
+import javafx.fxml.FXML;
+
 public class LoginController {
-    
+    @FXML
+    private void handleLogin(){
+        System.out.println("login");
+    }
 }
